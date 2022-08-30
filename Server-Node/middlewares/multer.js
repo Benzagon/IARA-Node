65ee5a4a6c11f96f0c4d48a7557da794bf05e9c6
LuisEmbonStrizzi <101569004+LuisEmbonStrizzi@users.noreply.github.com>
2022-08-30 13:53:59 -0300

    transfering files from monorepo colotadeo/lara to IARA-TBC/backend

---
+++ Server-Node/middlewares/multer.js
@@ -0,0 +1,14 @@
+//Middleware que nos permitirá configurar donde se guardará la imagen subida 
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+    destination: 'images',
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+export const upload = multer({
+    storage,
+}).single('file');
